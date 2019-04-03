f6be09a9b27c38fec503835907407d39c4a34b01
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-04-03 19:04:02 +0900

    Implemented image defaults for portfolio items in the model.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,5 +5,12 @@ class Portfolio < ApplicationRecord
     where(subtitle: "Angular") #Portfolio.where(subtitle: "Angular")
   end
 
-  scope: :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  # scope: :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }   THERES A SYNTAX ERROR HERE
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400.jpg" #the pipes make it so you don't override main images you already put in.
+    self.thumb_image ||= "https://via.placeholder.com/350x200.jpg"
+  end
 end
