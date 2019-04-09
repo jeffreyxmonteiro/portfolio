a18074a1924837f358254b90e5a396f4d1edfa03
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-04-09 16:49:34 +0900

    Implemented the content tag helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,12 @@ module ApplicationHelper
        (link_to "Login",  new_user_session_path)
      end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}!"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
 end
 
