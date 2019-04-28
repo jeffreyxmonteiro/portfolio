1505253ec082b0a1404e45c25c7fe26ac68a7c31
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-04-27 21:24:32 -0400

    Implemented refactor for navigation and active statuses.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,5 +19,43 @@ module ApplicationHelper
   def copyright_generator
     MonteiroViewTool::Renderer.copyright('Jeff Monteiro', 'All rights reserved')
   end
-end
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
+
+  def nav_helper(style, tag)
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+end
